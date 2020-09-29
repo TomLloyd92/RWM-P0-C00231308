2427c993d082ffa2cf6b7c1093b9f95035725c40
Tom Lloyd <c00231308@itcarlow.ie>
2020-09-29 11:52:11 +0100

    XOR21 test passing

---
+++ P0/Assets/Scripts/XOR21.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XOR21
+{
+  public static int XOR(int x)
+  {
+        return x ^ 21;
+  }
+
+  public static int[] XORcalculation(int[] xs)
+  {
+    int[] result = new int[xs.Length];
+    for (int i = 0; i < xs.Length; i++)
+    {
+      result[i] = XOR(xs[i]);
+    }
+    return result;
+  }
+}
+
---
+++ P0/Assets/Tests/XOR21test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class XOR21test
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 4, 2, 8, 3, 9, 4, 10 };
+            int[] output = XOR21.XORcalculation(input);
+            int[] expected = { 17, 23, 29, 22, 28, 17, 31 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
