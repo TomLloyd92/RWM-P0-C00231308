8cc99a8e2f5074339d2ce76ae2e4be7e750898a1
TomLloydGameDev <tomlloydgamedev@gmail.com>
2020-09-29 10:03:58 +0100

    Base Tests Working

---
+++ P0/Assets/Tests/CubeFilterTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CubeFilterTest
+    {
+        [Test]
+        public void CubeFilterSimple()
+        {
+            int[] input = { 1, 3, 5, 7, 9, 999 };
+            int[] output = CubeFilter.cubes(input);
+            int[] expected = { 1, 27, 125, 343, 729, 997002999 };
+
+            foreach (var o in output)
+            {
+                Debug.Log(o);
+            }
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
+
